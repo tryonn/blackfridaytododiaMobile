fd7a73b10d894abb1ad5e4330457628eed7b748d
Simão Menezes <simaomenezesneto@gmail.com>
2017-01-30 17:52:21 -0200

    Adicionando barra de busca, implementando a mesma, busca na lista que é carregada.

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -18,6 +18,9 @@ import { InAppBrowser } from 'ionic-native';
 export class Page1 {
 
   public feeds: Array<string>;
+  public noFilter: Array<any>;
+  public hasFilter: boolean = false;
+  public searchTerm: string = '';
   
   //private url: string = "https://www.reddit.com/new.json";
   private url: string = "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fblackfridaytododia.com%2Ffeed%2F";
@@ -36,6 +39,8 @@ export class Page1 {
     this.http.get(this.url).map(res => res.json()).subscribe(data => {
         this.feeds = data.items;        
         console.log(this.feeds);
+
+        this.noFilter = this.feeds;//noFilter rece dados da lista
         loading.dismiss();
 
     });
@@ -44,6 +49,14 @@ export class Page1 {
 
   itemSelected(url: string):void{
      let browser = new InAppBrowser(url, '_system');
+  }
+
+  // metodo para busca na lista
+  filterItems(){
+    this.feeds = this.noFilter.filter((item) => {
+      console.log(item);
+      return item.title.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+    });
   }
 
 }
