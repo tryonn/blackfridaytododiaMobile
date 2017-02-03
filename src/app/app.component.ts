34507a50cb5b86e8cc017ffdccaa63f06f30897f
Simão Menezes <simaomenezesneto@gmail.com>
2017-02-03 14:44:42 -0200

    Criando pagina da home, adicionando img de teste, criando pasta para imgs, adicionando grid e card na home.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { HomePage } from '../pages/home/home';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -21,8 +22,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Ofertas', component: Page1 },
+      { title: 'Page Two', component: Page2 },
+      { title: 'Home', component: HomePage}
     ];
 
   }
